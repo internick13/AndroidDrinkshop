aa2b5fa83f5c1cbc51caf320158c33ee912bed26
Jose Trinidad <internick18@gmail.com>
2019-02-21 16:26:20 -0600

    Commit added count boutton item

--- app/src/main/java/galosoft/com/androiddrinkshop/HomeActivity.java
+++ app/src/main/java/galosoft/com/androiddrinkshop/HomeActivity.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.nex3z.notificationbadge.NotificationBadge;
 
 import java.util.HashMap;
 import java.util.List;
@@ -45,6 +46,8 @@ public class HomeActivity extends AppCompatActivity
     IDrinkShopAPI mService;
 
     RecyclerView lst_menu;
+
+    NotificationBadge badge;
 
     //RX java
     CompositeDisposable compositeDisposable = new CompositeDisposable();
@@ -184,8 +187,26 @@ public class HomeActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        getMenuInflater().inflate(R.menu.menu_action_bar, menu);
+        View view  = menu.findItem(R.id.cart_menu).getActionView();
+        badge =  view.findViewById(R.id.badge);
+        updateCartCount();
         return true;
+    }
+
+    private void updateCartCount() {
+        if(badge == null) return;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+               if(Common.cartRepository.countCartItems() == 0) {
+                   badge.setVisibility(View.INVISIBLE);
+               } else {
+                   badge.setVisibility(View.VISIBLE);
+                   badge.setText(String.valueOf(Common.cartRepository.countCartItems()));
+               }
+            }
+        });
     }
 
     @Override
@@ -196,7 +217,7 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.cart_menu) {
             return true;
         }
 
@@ -226,5 +247,11 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateCartCount();
     }
 }
